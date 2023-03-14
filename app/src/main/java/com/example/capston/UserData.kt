422cf447791f09a0547ca16edf218d68b6b7cfc7
miller198 <miller198@hanyang.ac.kr>
2023-03-14 19:36:25 +0900

    Add data class file, UserData.kt

---
+++ app/src/main/java/com/example/capston/UserData.kt
@@ -0,0 +1,19 @@
+package com.example.capston
+
+data class UserData(
+    val username: String,
+    val email: String,
+    val pet_list: PetListModel,
+)
+
+data class PetListModel(
+    var pet_list : ArrayList<PetInfo>
+)
+
+data class PetInfo(
+    var pet_name : String? = null,
+    var breed: String? = null,
+    var gender : Int? = null, // female = 0, male = 1
+    var born : String? = null,
+//    var image_url : String? = null,
+)
